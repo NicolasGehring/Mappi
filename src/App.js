2eef122ef9236727df97a998893b226146afb8f3
Nicolas Gehring <n.gehring@online.de>
2019-08-14 10:44:40 +0200

    added .env gitignore and backend script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Navbar from "./components/navbar.component"
+import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
@@ -11,12 +12,12 @@ function App() {
   return (
     <Router>
       <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
