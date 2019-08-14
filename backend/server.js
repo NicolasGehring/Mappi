7b114031bafaaa6e88c421ed36fc1bffa058aa46
Nicolas Gehring <n.gehring@online.de>
2019-08-14 13:06:43 +0200

    removed unused node_moduels

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const graphqlHTTP = require("express-graphql");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+var { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  }
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
 
 app.use(cors());
 app.use(express.json());
@@ -25,5 +50,4 @@ app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
-  console.log(process);
 });
---
+++ src/components/Map/Map.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Map() {
+  return <div>Test from Map</div>;
+}
+
+export default Map;
