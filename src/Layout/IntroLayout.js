51713c51f819369b5c50837c9665f2b232c1dbdf
Nicolas Gehring <n.gehring@online.de>
2020-05-14 00:26:45 +0200

    added title

--- src/Layout/IntroLayout.js
+++ src/Layout/IntroLayout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import LocalBar from "@material-ui/icons/LocalBar";
 
 const useStyles = makeStyles({
   root: {
     height: "100vh",
-    backgroundColor: "green",
+  },
+  icon: {
+    fontSize: "70px",
   },
 });
 export default function IntroLayout() {
@@ -17,10 +21,19 @@ export default function IntroLayout() {
       className={classes.root}
       direction={"column"}
       alignContent={"stretch"}
-      justify={"space-between"}
+      justify={"space-around"}
       alignItems={"center"}
     >
-      <Grid item>Test</Grid>
+      <Grid item>
+        <Grid container direction={"row"}>
+          <Grid item>
+            <Typography variant={"h2"}>Corner Map</Typography>
+          </Grid>
+          <Grid item>
+            <LocalBar className={classes.icon} />
+          </Grid>
+        </Grid>
+      </Grid>
       <Grid item>Test</Grid>
       <Grid item>Test</Grid>
     </Grid>
